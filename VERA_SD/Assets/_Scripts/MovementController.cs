7a75d2a60315607dd485a258741f68e9ed32433e
joshfeds <joshfeds14@gmail.com>
2024-01-31 23:14:08 -0500

    added comments and movement speed adjustment

--- VERA_SD/Assets/_Scripts/MovementController.cs
+++ VERA_SD/Assets/_Scripts/MovementController.cs
@@ -15,7 +15,7 @@ public class MovementController : MonoBehaviour
     [Header("Movement")]
     [SerializeField] MovementInput _input;
 
-    [SerializeField] float speed = 1f;
+    [SerializeField] public float speed = 1f;
     [SerializeField] public float rotationValue = 15f;
 
     private void Awake(){
