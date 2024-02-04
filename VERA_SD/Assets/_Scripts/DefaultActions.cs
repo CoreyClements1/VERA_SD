9fe3cf07da3daea3407a43f345f8f090ea366436
ParsaB22 <parsabaghaie@gmail.com>
2024-02-03 19:11:01 -0500

    Updated Grabbing and Releasing object
    Now if XR Grab Interactable has an attach transform object on it then it will be able to grab in the desired orientation.
    
    Now Releasing Object will release it either 3f outwards or whatever collider you are looking at
    
    Object position is set to right infront of user just for testing right now.
    Also drop distance in set to 3f for testing can be changed for desired behavior later

--- VERA_SD/Assets/_Scripts/DefaultActions.cs
+++ VERA_SD/Assets/_Scripts/DefaultActions.cs
@@ -9,12 +9,11 @@ public class DefaultActions : MonoBehaviour
     // ExampleInteractions provides a simple sample interaction which can be triggered by interactables
     //DefaultActions uses ExampleInteractions as a base structure but uses More Relevent Default Functionality// Atleast that is the goal.
     #region VARIABLES
-    
+
 
     [SerializeField] private GrabTracker grabHandler;
     private bool isGrabbing = false;
     private Renderer rend;
-    // private GameObject grabbedObject;
 
     #endregion
 
@@ -37,19 +36,13 @@ public class DefaultActions : MonoBehaviour
             Debug.Log("GrabTracker found.");
         }
         rend = GetComponent<Renderer>();
+        // Debug.Log("localPosition"+transform.localPosition);
+        // Debug.Log("GlobalPosition"+transform.position);
     } // END Start
 
     private void Update()
     {
-        // grabHandler = GetComponent<GrabTracker>();
-        // if (grabHandler == null)
-        // {
-        //     Debug.LogError("GrabTracker is not assigned or not found.");
-        // }
-        // else
-        // {
-        //     Debug.Log("GrabTracker found.");
-        // }
+
     }
 
     #endregion
@@ -57,8 +50,10 @@ public class DefaultActions : MonoBehaviour
 
     #region SAMPLE INTERACTIONS
     //
-    public void GrabOrRelease(){
-        if(GetComponent<XRGrabInteractable>() != null){
+    public void GrabOrRelease()
+    {
+        if (GetComponent<XRGrabInteractable>() != null)
+        {
             if (isGrabbing == false)
             {
                 if (grabHandler.GetGrabbedObject() == null)
@@ -75,67 +70,137 @@ public class DefaultActions : MonoBehaviour
                 ReleaseObject();
             }
         }
-        else{
+        else
+        {
             Debug.LogError("Not A Grab Interactable");
         }
     }
     //
 
-    void GrabObject(GameObject obj){
+    void GrabObject(GameObject obj)
+    {
         isGrabbing = true;
         grabHandler.SetGrabbedObject(obj);
-        // grabbedObject = obj;
-
+        grabHandler.SetGrabParent(transform.parent);
         // Disable gravity during grab
         Rigidbody rb = obj.GetComponent<Rigidbody>();
-        if (rb != null){
+        if (rb != null)
+        {
             rb.useGravity = false;
             rb.freezeRotation = true;
+            rb.isKinematic = true;
         }
         // use camera rotation
+
+        XRGrabInteractable grabInteractable = obj.GetComponent<XRGrabInteractable>();
+        //use camera rotationXRGrabInteractable grabInteractable = obj.GetComponent<XRGrabInteractable>();
         Quaternion cameraRotation = Camera.main.transform.rotation;
-        // Apply the camera rotation to the grabbed object
-        obj.transform.rotation = cameraRotation;
+        Vector3 offset = new Vector3(0.5f, 0.5f, 0.5f); // Adjust the offset as needed
+        Vector3 targetPosition = Camera.main.ViewportToWorldPoint(offset);
+
+        if (grabInteractable != null && grabInteractable.attachTransform != null)
+        {
+            Quaternion grabRotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
+
+            grabInteractable.attachTransform.SetParent(Camera.main.transform, true);
+            transform.SetParent(grabInteractable.attachTransform, true);
+
+            grabInteractable.attachTransform.position = targetPosition;
+            grabInteractable.attachTransform.rotation = grabRotation;
+
+
+        }
+        else
+        {
+            Debug.LogWarning("XR Grab Interactable component or Attach Transform not found. Using default rotation.");
+            Quaternion grabRotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
+            transform.SetParent(Camera.main.transform, true);
+            // If XR Grab Interactable or Attach Transform is not available, use camera rotation
+            // obj.transform.localPosition = targetPosition;
+            // obj.transform.localRotation = grabRotation;
+            obj.transform.position = targetPosition;
+            obj.transform.rotation = grabRotation;
+
+        }
         // Snap the grabbed object to the bottom right of the player's camera
-        Vector3 offset = new Vector3(0.75f, 0f, 0.25f); // Adjust the offset as needed
-        obj.transform.position = Camera.main.ViewportToWorldPoint(offset);
         // }
     }
 
     // ReleaseObject method
-    void ReleaseObject(){
+    void ReleaseObject()
+    {
         isGrabbing = false;
         GameObject obj = grabHandler.GetGrabbedObject();
+        obj.transform.SetParent(grabHandler.GetGrabParent(), true);
         // Enable gravity during release
+        XRGrabInteractable grabInteractable = obj.GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null && grabInteractable.attachTransform != null)
+        {
+            // Debug.Log(grabInteractable.attachTransform);
+            grabInteractable.attachTransform.SetParent(obj.transform, true);
+        }
+
+        // Place the grabbed object in front of the player with smooth interpolation
+        // Vector3 center = new Vector3(0.5f, 0.5f, 0.0f);
+        Vector3 zero = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        // obj.transform.localRotation = Quaternion.identity;
+        Vector3 offset = new Vector3(0.5f, 0.5f, 3.0f); // Adjust the offset as needed// adjust for distance before drop
+        Vector3 targetPosition = Camera.main.ViewportToWorldPoint(offset);
+        // Check for collisions along the path
+        if (CheckCollisions(zero, targetPosition))
+        {
+            // If there is a collision, move to the point just before the collision
+            Vector3 adjustedPosition = FindAdjustedPosition(zero, targetPosition, obj);
+            obj.transform.position = adjustedPosition;
+        }
+        else
+        {
+            // If no collision, move to the target position
+            Debug.Log("Entered");
+            obj.transform.position = targetPosition;
+        }
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.useGravity = true;
             rb.freezeRotation = false;
+            rb.isKinematic = false;
         }
-        // Place the grabbed object in front of the player with smooth interpolation
-        Vector3 frontOffset = new Vector3(0f, 0f, 1.0f); // Adjust the offset as needed// adjust for distance before drop
-        Vector3 targetPosition = transform.position + transform.forward * frontOffset.z;
-
-        StartCoroutine(MoveObjectSmoothly(obj.transform, targetPosition, 0.75f)); // Adjust the duration as needed + makes it slower - makes it faster
-
         grabHandler.SetGrabbedObject(null);
     }
 
-    IEnumerator MoveObjectSmoothly(Transform objectTransform, Vector3 targetPosition, float duration){
-        float elapsedTime = 0f;
-        Vector3 initialPosition = objectTransform.position;
-
-        while (elapsedTime < duration)
-        {
-            objectTransform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / duration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        // objectTransform.position = targetPosition; // Ensure it reaches the exact target position
+    bool CheckCollisions(Vector3 start, Vector3 end)
+    {
+        // Check for collisions using Physics.Linecast
+        // This function returns true if there is a collision along the line
+        return Physics.Linecast(start, end);
     }
-    
+
+    Vector3 FindAdjustedPosition(Vector3 start, Vector3 end, GameObject obj)
+    {
+        RaycastHit hit;
+        Vector3 adjustedPosition = end;
+
+        // Perform a Raycast to find the first collision along the path
+        int ogLayer = obj.layer;
+        // Debug.Log("ogLayer; "+ogLayer);
+        obj.layer = LayerMask.NameToLayer("Ignore Raycast");
+        // Debug.Log("NamerToLayer; "+LayerMask.NameToLayer("Ignore Raycast"));
+        int layerMask = ~LayerMask.GetMask("Ignore Raycast");
+
+        // Collider[] relaventColliders = obj.GetComponentsInChildren<Collider>();
+
+        // Move to the point just before the collision
+        if (Physics.Raycast(start, (end - start), out hit, Vector3.Distance(start, end), layerMask))
+        {
+
+            // Adjust the offset as needed
+            adjustedPosition = adjustedPosition = hit.point - (end - start).normalized * 0.1f;
+        }
+        obj.layer = ogLayer;
+
+        return adjustedPosition;
+    }
     #endregion
 
 
--- VERA_SD/Assets/_Scripts/GrabTracker.cs
+++ VERA_SD/Assets/_Scripts/GrabTracker.cs
@@ -1,41 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
     private GameObject grabbedObject;
+    private Transform objParent;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(grabbedObject!=null){
-            UpdateGrabbedObjectPosition();
-        }
     }
 
-    public void SetGrabbedObject(GameObject obj){
+    public void SetGrabParent(Transform obj)
+    {
+        objParent = obj;
+    }
+
+    public Transform GetGrabParent()
+    {
+        return objParent;
+    }
+    public void SetGrabbedObject(GameObject obj)
+    {
         grabbedObject = obj;
-        // Debug.LogError(grabbedObject);
     }
 
-    public GameObject GetGrabbedObject(){
+    public GameObject GetGrabbedObject()
+    {
         return grabbedObject;
-    }
-    
-    void UpdateGrabbedObjectPosition(){
-        // Update the position of the grabbed object to stay at the bottom right of the player' camera
-        Vector3 offset = new Vector3(0.75f, 0f, 0.25f); // Adjust the offset as needed
-
-        //use camera rotation
-        Quaternion cameraRotation = Camera.main.transform.rotation;
-        // Apply the camera rotation to the grabbed object
-        grabbedObject.transform.rotation = cameraRotation;
-        grabbedObject.transform.position = Camera.main.ViewportToWorldPoint(offset);
     }
 }
