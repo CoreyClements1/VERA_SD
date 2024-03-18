724201a3f342b8ac7b66c0ae8014f24902fc8bf6
ParsaB22 <parsabaghaie@gmail.com>
2024-03-18 19:12:30 -0400

    Update Looking.cs

--- VERA_SD/Assets/Looking.cs
+++ VERA_SD/Assets/Looking.cs
@@ -7,25 +7,83 @@ public class Looking : MonoBehaviour
     // Start is called before the first frame update
     // public GameObject camera;
     public float speed;
-    public float smooth;
-    public float minAngle = 90f;
-    public float maxAngle = -90f;
+    // public float smooth;
+    public float upAngle = 90;
+
+    public float downAngle = 90;
+    private float adjustedSpeed;
     void Start()
     {
-        
+        adjustedSpeed = speed;
+        upAngle = 360 - upAngle;
+        if (downAngle > 90)
+        {
+            downAngle = 90;
+        }
+        if (upAngle < 270)
+        {
+            upAngle = 270;
+        }
     }
-    public void LookUp(){
-        // if( camera.transform.eulerAngles.x > -180 && camera.transform.eulerAngles.x < 180){
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x-speed, transform.rotation.y, transform.rotation.z),  smooth);
-        // }
-        transform.Rotate(-speed, 0f, 0f, Space.Self);
-    }
-    public void LookDown(){
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x+speed, transform.rotation.y, transform.rotation.z),smooth);
-        transform.Rotate(speed, 0f, 0f, Space.Self);
-    }
-    public void ResetAngle(){
-        transform.rotation = Quaternion.Euler(0, 0, 0);
+    //if max or min is set greater than 90 then i believe it will break
+    public void LookUp()
+    {
+        // Debug.Log(transform.rotation);
+        float currentAngle;
+        if (transform.rotation.eulerAngles.x == 0)
+        {
+            currentAngle = 360;
+        }
+        else
+        {
+            currentAngle = transform.rotation.eulerAngles.x;
+        }
 
+        if (currentAngle % 360 <= downAngle || currentAngle % 360 > upAngle)
+        {
+            if (currentAngle == downAngle)
+            {
+                transform.Rotate(-adjustedSpeed, 0f, 0f, Space.Self);
+            }
+            else
+            {
+                if ((currentAngle - speed) % 360 > downAngle && (currentAngle % 360 - speed) < upAngle)
+                {
+                    adjustedSpeed = (currentAngle) - upAngle;
+                }
+                else
+                {
+                    adjustedSpeed = speed;
+                }
+                transform.Rotate(-adjustedSpeed, 0f, 0f, Space.Self);
+
+            }
+        }
+    }
+    public void LookDown()
+    {
+        if ((transform.rotation.eulerAngles.x % 360) < downAngle || (transform.rotation.eulerAngles.x % 360) >= upAngle)
+        {
+            if (transform.rotation.eulerAngles.x == upAngle)
+            {
+                transform.Rotate(adjustedSpeed, 0f, 0f, Space.Self);
+            }
+            else
+            {
+                if ((transform.rotation.eulerAngles.x % 360 + speed) > downAngle && (transform.rotation.eulerAngles.x % 360 + speed) < upAngle)
+                {
+                    adjustedSpeed = downAngle - (transform.rotation.eulerAngles.x);
+                }
+                else
+                {
+                    adjustedSpeed = speed;
+                }
+                transform.Rotate(adjustedSpeed, 0f, 0f, Space.Self);
+            }
+        }
+    }
+    public void ResetAngle()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
