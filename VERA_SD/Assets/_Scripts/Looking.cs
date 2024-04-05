d8c8f0d887d5be279ec09bdf56c6fe4f656aaa8d
CoreyClements1 <coreyclements@knights.ucf.edu>
2024-04-04 21:50:47 -0400

    Looking

---
+++ VERA_SD/Assets/_Scripts/Looking.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public GameObject camera;
+    public float speed;
+    public float smooth;
+    public float minAngle = 90f;
+    public float maxAngle = 90f;
+    private Transform mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        
+
+    }
+
+    public void LookUp()
+    {
+        if (Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 <= 90 || Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 > 270)
+        {
+            Vector3 oldPos = mainCam.parent.position;
+            Camera.main.transform.parent.parent.Rotate(-speed, 0f, 0f, Space.Self);
+            mainCam.parent.position = oldPos;
+        }
+
+    }
+
+    public void LookDown()
+    {
+        if (Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 < 90 || Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 >= 270)
+        {
+            Vector3 oldPos = mainCam.parent.position;
+            Camera.main.transform.parent.parent.Rotate(speed, 0f, 0f, Space.Self);
+            mainCam.parent.position = oldPos;
+        }
+    }
+
+    public void ResetAngle()
+    {
+        Vector3 oldPos = mainCam.parent.position;
+        Camera.main.transform.parent.parent.rotation = Quaternion.Euler(0, mainCam.parent.rotation.eulerAngles.y, mainCam.parent.rotation.eulerAngles.z);
+        mainCam.parent.position = oldPos;
+    }
+}
