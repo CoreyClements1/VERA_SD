151ea2c9e684ab924388e56911436d78ce0b92f1
ParsaB22 <parsabaghaie@gmail.com>
2024-04-09 20:41:41 -0400

    Reverted looking back
    ive given up. yet i was so close

--- VERA_SD/Assets/_Scripts/Looking.cs
+++ VERA_SD/Assets/_Scripts/Looking.cs
@@ -5,109 +5,40 @@ using UnityEngine;
 public class Looking : MonoBehaviour
 {
     // Start is called before the first frame update
+    // public GameObject camera;
     public float speed;
-    [SerializeField][Range(0, 90)] private float upAngle = 90;
-    [SerializeField][Range(0, 90)] private float downAngle = 90;
-    private float adjustedSpeed;
+    public float smooth;
+    public float minAngle = 90f;
+    public float maxAngle = 90f;
     private Transform mainCam;
-    private float newVertChange;
-    private float verticleAngle;
+
     void Start()
     {
-        mainCam = Camera.main.transform.parent;
-        adjustedSpeed = speed;
-        upAngle = 360 - upAngle;
+        mainCam = Camera.main.transform;
     }
-    //if max or min is set greater than 90 then i believe it will break
-    void Update()
+
+    private void LateUpdate()
     {
-        float radians = (Camera.main.transform.localRotation.eulerAngles.y * Mathf.PI) / 180;
-        float cos = Mathf.Cos(radians);
-        float sin = Mathf.Sin(radians);
-        Vector3 newVector = new Vector3(sin, 0f, cos);
-        Debug.Log("vector: " + newVector);
-        Debug.Log("cameraForward: " + Camera.main.transform.forward);
-        Vector3 rightVect = Vector3.Cross(newVector, Vector3.up).normalized;
-        Debug.Log("rightVect: " + rightVect);
-        Debug.Log("cameraRight: " + Camera.main.transform.right);
-        mainCam.rotation = Quaternion.Euler(0f, 0f, 0f);
-        mainCam.RotateAround(Camera.main.transform.position, rightVect, verticleAngle);
     }
+
     public void LookUp()
     {
-        if (verticleAngle < 90)
+        if (Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 <= 90 || Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 > 270)
         {
-            if (verticleAngle == -90)
-            {
-                verticleAngle += newVertChange;
-            }
-            else
-            {
-                if (verticleAngle + speed > 90)
-                {
-                    newVertChange = 90 - verticleAngle;
-                    verticleAngle = 90;
-                }
-                else
-                {
-                    verticleAngle += speed;
-                    newVertChange = speed;
-                }
-            }
+            Vector3 oldPos = mainCam.parent.position;
+            Camera.main.transform.parent.parent.Rotate(-speed, 0f, 0f, Space.Self);
+            mainCam.parent.position = oldPos;
         }
-        float radians = (Camera.main.transform.localRotation.eulerAngles.y * Mathf.PI) / 180;
-        float cos = Mathf.Cos(radians);
-        float sin = Mathf.Sin(radians);
-        Vector3 newVector = new Vector3(sin, 0f, cos);
-        Debug.Log("vector: " + newVector);
-        Debug.Log("cameraForward: " + Camera.main.transform.forward);
-        Vector3 rightVect = Vector3.Cross(newVector, Vector3.up).normalized;
-        Debug.Log("rightVect: " + rightVect);
-        Debug.Log("cameraRight: " + Camera.main.transform.right);
-        mainCam.rotation = Quaternion.Euler(0f, 0f, 0f);
-        mainCam.RotateAround(mainCam.position, rightVect, verticleAngle);
+
     }
+
     public void LookDown()
     {
-        //Comments below are for logic of the throwing rotation stuff
-        //CURRENT>-90
-        if (verticleAngle > -90)
+        if (Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 < 90 || Camera.main.transform.parent.parent.rotation.eulerAngles.x % 360 >= 270)
         {
-            if (verticleAngle == 90)
-            {
-                verticleAngle -= newVertChange;
-            }
-            else
-            {
-                if (verticleAngle - speed < -90)
-                {
-                    newVertChange = 90 + verticleAngle;
-                    verticleAngle = -90;
-                }
-                else
-                {
-                    verticleAngle -= speed;
-                    newVertChange = speed;
-                }
-            }
-
+            Vector3 oldPos = mainCam.parent.position;
+            Camera.main.transform.parent.parent.Rotate(speed, 0f, 0f, Space.Self);
+            mainCam.parent.position = oldPos;
         }
-        float radians = (Camera.main.transform.localRotation.eulerAngles.y * Mathf.PI) / 180;
-        float cos = Mathf.Cos(radians);
-        float sin = Mathf.Sin(radians);
-        Vector3 newVector = new Vector3(sin, 0f, cos);
-        Debug.Log("vector: " + newVector);
-        Debug.Log("cameraForward: " + Camera.main.transform.forward);
-        Vector3 rightVect = Vector3.Cross(newVector, Vector3.up).normalized;
-        Debug.Log("rightVect: " + rightVect);
-        Debug.Log("cameraRight: " + Camera.main.transform.right);
-        mainCam.rotation = Quaternion.Euler(0f, 0f, 0f);
-        mainCam.RotateAround(mainCam.position, rightVect, verticleAngle);
-    }
-    public void ResetAngle()
-    {
-        verticleAngle = 0;
-        newVertChange = speed;
-        mainCam.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
