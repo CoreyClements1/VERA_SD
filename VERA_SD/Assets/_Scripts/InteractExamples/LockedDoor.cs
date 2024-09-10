c010a5a6b96bf776297670935ac57c9028836d67
CoreyClements1 <co430015@ucf.edu>
2024-09-09 20:11:46 -0400

    Ready for VERA Integration
    Ready for formal integration into the parent VERA framework

---
+++ VERA_SD/Assets/_Scripts/InteractExamples/LockedDoor.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+
+
+    #region VARIABLES
+
+
+    [SerializeField] private Key linkedKey;
+    [SerializeField] private VLAT_DefaultInteractions leftDoor;
+    [SerializeField] private VLAT_DefaultInteractions rightDoor;
+    private GrabTracker grabHandler;
+    private bool locked = true;
+
+
+    #endregion
+
+
+    #region MONOBEHAVIOUR
+
+
+    // Start
+    //--------------------------------------//
+    private void Start()
+    //--------------------------------------//
+    {
+        grabHandler = FindObjectOfType<GrabTracker>();
+
+    } // END Start
+
+
+    #endregion
+
+
+    #region OPEN
+
+
+    // Tries to open/close the locked door, depending on if key is held / unlocked
+    //--------------------------------------//
+    public void TryOpenCloseLockDoor()
+    //--------------------------------------//
+    {
+        if (!locked)
+        {
+            leftDoor.DoorOpenAndClose();
+            rightDoor.DoorOpenAndClose();
+        }
+        else
+        {
+            if (grabHandler.grabbedObject != null)
+            {
+                if (grabHandler.grabbedObject == linkedKey.gameObject)
+                {
+                    linkedKey.Unlock();
+                    locked = false;
+                    leftDoor.DoorOpenAndClose();
+                    rightDoor.DoorOpenAndClose();
+                }
+            }
+        }
+
+    } // END TryOpenCloseLockDoor
+
+
+    #endregion
+
+
+} // END LockedDoor.cs
